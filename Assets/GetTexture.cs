79145cffecca73e463fa2ef47449838c8b3f40e4
Ptit-Biscuit <vincent.brebion@epsi.fr>
2020-03-12 22:39:12 +0100

    Animations and buttons

---
+++ Assets/GetTexture.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetTexture : MonoBehaviour {
+
+    private RegionCapture regionCapture;
+    private Camera RenderTextureCamera;
+    [Space(20)]
+    public GameObject Region_Capture;
+    public bool FreezeEnable = false;
+    private static readonly int MainTex = Shader.PropertyToID("_MainTex");
+
+    void Start () {
+        RenderTextureCamera = Region_Capture.GetComponentInChildren<Camera>();
+
+        regionCapture = Region_Capture.GetComponent<RegionCapture> ();
+
+        Console.Out.WriteLine("Starting region capture !");
+        
+        if (FreezeEnable) {
+            regionCapture.Check_OutOfBounds = true;
+        }
+
+        StartCoroutine(WaitForTexture());
+
+    }
+
+    private IEnumerator WaitForTexture() {
+
+        yield return new WaitForEndOfFrame ();
+
+        if (RenderTextureCamera.targetTexture) {
+            GetComponent<Renderer> ().material.SetTexture (MainTex, RenderTextureCamera.targetTexture);
+        }
+        else StartCoroutine(WaitForTexture());
+
+    }
+
+    void LateUpdate () {
+
+        // if (FreezeEnable && regionCapture.)
+        //     RenderTextureCamera.enabled = false;
+
+        // RenderTextureCamera.enabled = true;
+    }
+}
